32977232b08a9590ed582adc238405cbb0f4dd10
NiroshkumarGN <niroshkumar1606@gmail.com>
2022-05-08 21:39:30 +0530

    leavemanagement app

---
+++ src/main/java/com/leavemanagementsystem/LeavemanagementsystemApplyleaveMsApiApplication.java
@@ -0,0 +1,13 @@
+package com.leavemanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeavemanagementsystemApplyleaveMsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeavemanagementsystemApplyleaveMsApiApplication.class, args);
+	}
+
+}
